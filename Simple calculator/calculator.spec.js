648b60e0b2f5c8b3cc6dd0c2d28028f0c4395268
Saloni Sharma <sharmasaloni061@gmail.com>
2019-10-19 21:02:08 +0530

    Specs

--- Simple calculator/calculator.spec.js
+++ Simple calculator/calculator.spec.js
@@ -1,3 +1,18 @@
 describe('calculator.js',function() {
+    it('should add numbers to total', function() {
 
+    });
+
+    it('should subtract numbers from total', function() {
+
+    });
+
+    it('should multiply total by number', function() {
+
+    });
+
+    it('should divide total by number', function() {
+
+    });
+    
 });
