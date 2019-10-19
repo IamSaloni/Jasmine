6fb6008723eef4b5ad2dbd1a65a008084e85ccaa
Saloni Sharma <sharmasaloni061@gmail.com>
2019-10-19 21:26:21 +0530

    Added Expectations in the suites

--- Simple calculator/calculator.spec.js
+++ Simple calculator/calculator.spec.js
@@ -1,18 +1,29 @@
 describe('calculator.js',function() {
     it('should add numbers to total', function() {
-
+        const calculator = new Calculator();
+        calculator.add(5);
+        expect(calculator.total).toBe(5);
     });
 
     it('should subtract numbers from total', function() {
-
+        const calculator =  new Calculator();
+        calculator.total = 30;
+        calculator.subtract(20);
+        expect(calculator.total).toBe(10);
     });
 
     it('should multiply total by number', function() {
-
+        const calculator = new Calculator();
+        calculator.total = 10;
+        calculator.multiply(5);
+        expect(calculator.total).toBe(50);
     });
 
     it('should divide total by number', function() {
-
+        const calculator = new Calculator();
+        calculator.total = 10;
+        calculator.divide(10);
+        expect(calculator.total).toBe(1);
     });
     
 });
