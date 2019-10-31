cedae4fd6e25ce6d00b61eef2e9e0fd43ba97185
Saloni Sharma <sharmasaloni061@gmail.com>
2019-10-31 19:59:55 +0530

    beforeAll, afterAll

--- Simple calculator/main.spec.js
+++ Simple calculator/main.spec.js
@@ -12,20 +12,20 @@ describe("main.js", function() {
   describe("updateResult()", function() {
     let element;
     beforeAll(function() {
-      element = document.createElement("div");
+      const element = document.createElement("div");
       element.setAttribute("id", "result");
       document.body.appendChild(element);
+      this.element = element;
     });
 
     afterAll(function() {
-      const element = document.getElementById("result");
-      document.body.removeChild(element);
+      document.body.removeChild(this.element);
     });
 
     it("adds result to DOM element", function() {
 
       updateResult('5');
-      expect(element.innerText).toBe('5');
+      expect(this.element.innerText).toBe('5');
     });
   });
 });
