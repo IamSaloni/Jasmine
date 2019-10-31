846be63b90da56c54a61a337ac570fa5aaa6f421
Saloni Sharma <sharmasaloni061@gmail.com>
2019-10-31 13:44:10 +0530

    toMatch

--- Simple calculator/calculator.spec.js
+++ Simple calculator/calculator.spec.js
@@ -91,4 +91,13 @@ describe('calculator.js',function() {
         expect(function() {calculator.divide(0)}).toThrowError(Error);
       //  expect(function() {calculator.divide(0)}).toThrowError(Error, 'cannot divide by zer'); // it will throw error
     });
+
+    it('returns total', function() {
+        const calculator = new Calculator();
+        calculator.total = 50;
+
+        expect(calculator.add(20)).toBe(70);
+        expect(calculator.total).toMatch(/-?\d+/);
+        expect(typeof calculator.total).toMatch('number');
+    })
 });
